161774126e40df4f26894d70e82fab580e99430a
dragosiordachioaia <xdragosy@gmail.com>
2017-12-14 19:36:33 +0000

    fixed procfile and app

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import (Flask, jsonify)
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     return jsonify({"message": "You need to request a resource"}), 404
+
+if __name__ == "__main__":
+    app.run()
