89816485c3400dddb909e3dd63cc158a8152d73f
dragosiordachioaia <xdragosy@gmail.com>
2017-12-14 19:40:17 +0000

    added commented out code

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import (Flask, jsonify)
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return jsonify({"message": "You need to request a resource"}), 404
+    return "Hello World!"
+
+
+# @app.route('/<name>')
+# def home_name(name):
+#     return "Hello World! And hello to you too, {}!".format(name)
+
 
 if __name__ == "__main__":
     app.run()
